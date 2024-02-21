1a34ba105fd340e2431ce9fbd8474fe726799edf
Ryan McCusker <ryan@ryan-laptop-2.local>
2024-02-21 18:03:59 -0500

    fetching data; the cards look real weird

--- app/src/main/java/com/example/a491/ProfileActivity.kt
+++ app/src/main/java/com/example/a491/ProfileActivity.kt
@@ -2,10 +2,33 @@ package com.example.a491
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class ProfileActivity : AppCompatActivity() {
+    val renting_items = mutableListOf<Item>()
+    val listing_items = mutableListOf<Item>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
+
+        val renting_recycler = findViewById<RecyclerView>(R.id.rentingRecycler)
+        val listing_recycler = findViewById<RecyclerView>(R.id.listingRecycler)
+
+        renting_recycler.layoutManager = LinearLayoutManager(this, RecyclerView.HORIZONTAL, false)
+        listing_recycler.layoutManager = LinearLayoutManager(this, RecyclerView.HORIZONTAL, false)
+
+        val renting_adapter = ItemRecyclerViewAdapter(renting_items, this)
+        val listing_adapter = ItemRecyclerViewAdapter(listing_items, this)
+
+        renting_recycler.adapter = renting_adapter
+        listing_recycler.adapter = listing_adapter
+
+        val renting_fetcher = ItemFetcher(renting_items, renting_adapter)
+        val listing_fetcher = ItemFetcher(listing_items, listing_adapter)
+        renting_fetcher.getRentingItems()
+        listing_fetcher.getListingItems()
+
+
     }
 }
