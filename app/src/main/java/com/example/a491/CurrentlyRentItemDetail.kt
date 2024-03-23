db269d97dc0dac366a6b4187cdec00f3a5f13592
Ryan McCusker <ryan@ryan-laptop.home>
2024-03-23 18:33:44 -0400

    create return instance

--- app/src/main/java/com/example/a491/CurrentlyRentItemDetail.kt
+++ app/src/main/java/com/example/a491/CurrentlyRentItemDetail.kt
@@ -1,11 +1,13 @@
 package com.example.a491
 
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import com.example.a491.api.RetrofitClient
@@ -13,6 +15,8 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 
 class CurrentlyRentItemDetail :AppCompatActivity() {
     private lateinit var itemImageView : ImageView
@@ -24,6 +28,7 @@ class CurrentlyRentItemDetail :AppCompatActivity() {
     private lateinit var itemTipAmount: TextView
     private lateinit var itemStatus: TextView
     private lateinit var returnButton: Button
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.currently_renting_item_detail)
@@ -74,6 +79,7 @@ class CurrentlyRentItemDetail :AppCompatActivity() {
         returnButton.setOnClickListener {
             GlobalScope.launch(Dispatchers.Main) {
                 setItemAvailable(item)
+                createReturn(item)
             }
         }
 
@@ -100,8 +106,29 @@ class CurrentlyRentItemDetail :AppCompatActivity() {
     }
 
 
-    suspend fun setRentalReturned() {
+    @RequiresApi(Build.VERSION_CODES.O)
+    suspend fun createReturn(item: Item) {   //if you get bad request error, it might be because a return already exists with that rental id
+        val today = LocalDate.now()
+        val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd")
+        val date = today.format(formatter)
+        val newReturn = Return(
+            rental_id = item.rental_id,
+            location_of_lister = item.itemListerLocation,
+            location_of_renter = item.itemRenterLocation,
+            tip_amount = item.itemTipAmount,
+            return_date = date
+        )
+        Log.d("Huh", "id: ${newReturn.rental_id.toString()}, location1: ${newReturn.location_of_lister}, location: ${newReturn.location_of_renter}, tip: ${newReturn.tip_amount}, date: ${newReturn.return_date}")
 
+        val apiService = RetrofitClient.instance.create(ApiService::class.java)
+        try {
+            apiService.createReturn(newReturn)
+
+            Log.d("API", "Return created successfully")
+        } catch (e: Exception) {
+            Log.e("API", "Return could not be created")
+            Log.e("API", "Error: ${e.message}", e)
+        }
     }
     suspend fun setItemAvailable(item: Item) {
         val apiService = RetrofitClient.instance.create(ApiService::class.java)
